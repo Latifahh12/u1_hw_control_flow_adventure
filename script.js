e11e9589ed333db90e3e7a876ea8a21f98485852
Latifah Hutchings <latifahbusprofile@gmail.com>
2022-06-29 12:31:57 -0600

    i am here

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// prompts user and stores value in the variable
+let valueOfPrompt = prompt()
+// logs value stored
+console.log(valueOfPrompt)
 
+let age = prompt('How old are you?')
+// ES6 String Interpolation
+alert(`You are ${age} years old.`)
+// ES5 Version
+alert('You are ' + age + ' years old.')
